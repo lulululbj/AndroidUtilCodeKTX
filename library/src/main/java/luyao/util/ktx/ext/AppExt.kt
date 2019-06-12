b52ad830bf528b94c06803c473bc8c49eb7940c9
luyao <sunluyao1993x@gmail.com>
2019-06-12 13:16:27 +0800

    update AppExt
    * get ApkInfo by apk file

---
+++ library/src/main/java/luyao/util/ktx/ext/AppExt.kt
@@ -0,0 +1,38 @@
+package luyao.util.ktx.ext
+
+import android.content.Context
+import android.content.pm.PackageManager
+import luyao.util.ktx.bean.AppInfo
+import java.io.File
+
+/**
+ * Created by luyao
+ * on 2019/6/12 10:53
+ */
+
+/**
+ * get [AppInfo] by apk file path
+ */
+fun Context.getAppInfo(apkPath: String): AppInfo {
+    val packageInfo = packageManager.getPackageInfo(apkPath,PackageManager.GET_META_DATA)
+    packageInfo.applicationInfo.sourceDir=apkPath
+    packageInfo.applicationInfo.publicSourceDir=apkPath
+
+    val packageName = packageInfo.packageName
+    val appName =packageManager.getApplicationLabel(packageInfo.applicationInfo).toString()
+    val versionName = packageInfo.versionName
+    val versionCode = packageInfo.longVersionCode
+    val icon = packageManager.getApplicationIcon(packageInfo.applicationInfo)
+    return AppInfo(apkPath,packageName,versionName,versionCode,appName,icon)
+}
+
+/**
+ * get [AppInfo] list by apk folder path
+ */
+fun Context.getAppInfos(apkFolderPath:String):List<AppInfo>{
+    val appInfoList = ArrayList<AppInfo>()
+    for (file in File(apkFolderPath).listFiles())
+        appInfoList.add(getAppInfo(file.path))
+    return appInfoList
+}
+
